d7d1a8df7e4ef1e973273339d70a2b02dc074c8e
linedj <eunjun998@gmail.com>
2025-01-27 10:47:04 +0900

    post Dto apply

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -1,5 +1,6 @@
 package com.example.rest.domain.post.post.controller;
 
+import com.example.rest.domain.post.post.dto.PostDto;
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
 import com.example.rest.global.dto.RsData;
@@ -24,8 +25,12 @@ public class ApiV1PostController {
     }
 
     @GetMapping("{id}")
-    public Post getItem(@PathVariable long id) {
-        return postService.getItem(id).get();
+    public PostDto getItem(@PathVariable long id) {
+        Post post = postService.getItem(id).get();
+
+        PostDto postDto = new PostDto(post);
+
+        return postDto;
     }
 
 
