d281a0ceb0ac3634dbde9a4aa5ca807e7da3a6b0
linedj <eunjun998@gmail.com>
2025-01-31 10:45:37 +0900

    exception handler

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -11,7 +11,6 @@ import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -39,16 +38,7 @@ public class ApiV1PostController {
     @GetMapping("{id}")
     public RsData<PostDto> getItem(@PathVariable long id) {
 
-        Post post = null;
-        try {
-            post = postService.getItem(id).get();
-        } catch(NoSuchElementException e) {
-            return new RsData<>(
-                    "404-1",
-                    "%d번 글이 존재하지 않습니다.".formatted(id),
-                    null
-            );
-        }
+        Post post = postService.getItem(id).get();
 
         return new RsData<>(
                 "200-1",
